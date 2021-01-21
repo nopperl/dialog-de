1aa7e56d79afb1a8a14b38dd24970c8d3439ce41
nikuya3 <54780682+nikuya3@users.noreply.github.com>
2021-01-21 01:32:42 +0100

    Include readme in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.ifconfig',
               'sphinx.ext.viewcode',
               'sphinx.ext.githubpages',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',
+              'm2r2']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
